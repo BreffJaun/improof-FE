b4d8569c856acef264146678a078e72deba21917
Florian <f.mewes87@gmail.com>
2023-01-30 13:30:09 +0100

    initial commit

---
+++ src/api/host.jsx
@@ -0,0 +1,2 @@
+const host = "http://localhost:2404"
+export {host}
---
+++ src/components/Login.jsx
@@ -0,0 +1,56 @@
+const INITIAL_TALENT = {
+  profile:{
+    firstName: "", 
+    lastName: "", 
+    email: "", 
+    password: "", 
+    confirmPass: "only FE-check", 
+    isTalent: false, 
+    isRecruiter: false, 
+    colorSchema: "",
+    description: "",
+    goal: "",
+    position:"",
+    toolsAndSkills: "",
+  },
+  location:{
+    street:"",
+    zip:"",
+    city:""
+  },    
+  contact: {
+    mobile: "",
+    website: "",
+    online1:"",
+    online2:"",
+    online3:""
+  }
+}
+const INITIAL_RECRUITER = {
+  profile:{
+    firstName: "", 
+    lastName: "", 
+    email: "", 
+    password: "", 
+    confirmPw: "only FE-check", 
+    isTalent: false, 
+    isRecruiter: false, 
+    colorSchema: "",
+    description: "",
+    goal: "",
+    position:"",
+    toolsAndSkills: "",
+  },
+  location:{
+    street:"",
+    zip:"",
+    city:""
+  },    
+  contact: {
+    mobile: "",
+    website: "",
+    online1:"",
+    online2:"",
+    online3:""
+  }
+}
