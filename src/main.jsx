f0de7f8dd308c204bcd5fc69945985297fbc024f
grossesbewirken <grossesbewirken@gmail.com>
2023-01-31 09:32:51 +0100

    browser router installiert

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
