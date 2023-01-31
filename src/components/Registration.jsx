7d8ec5d19c5c7d40e76eff9be5e56511a17658c1
Florian <f.mewes87@gmail.com>
2023-01-31 13:44:48 +0100

    Datenstruktur im registrationObjekt angepasst

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -20,21 +20,21 @@ const Registration = () => {
     const [registrationData, setRegistrationData] = useState({profile:{}})
 
     const handleInput = (event) => {
-        setRegistrationData({...registrationData, [profile.event.target.name]: event.target.value});
+        setRegistrationData({...registrationData, profile:{ ...registrationData.profile,
+            [event.target.name]: event.target.value
+        }
+        });
     }
 
     const handleTalent = (event) => {
-        console.log(event.target)
-        setRegistrationData({...registrationData, [profile.isTalent]: true, [profile.isRecruiter]: false});
+        setRegistrationData({...registrationData, profile:{...registrationData.profile, isTalent: true, isRecruiter: false}});
     }
     const handleRecruiter = (event) => {
-        console.log(event.target)
-        setRegistrationData({...registrationData, [profile.isRecruiter]: true,  [profile.isTalent]: false});
+        setRegistrationData({...registrationData, profile:{...registrationData.profile, isRecruiter: true,  isTalent: false}});
     }
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-
         const sendRegistrationData = async ()  => {
         await fetch(`${host}/users/add`, {
             method: 'POST',
@@ -53,31 +53,46 @@ const Registration = () => {
                 }
             });
         };
-        sendRegistrationData();
+  
+        if( registrationData.profile.password !== registrationData.profile.confirmPassword){
+            toast.info("confirm password an password are not equal", toastOptions)
+        }else if (!registrationData.profile.isRecruiter && !registrationData.profile.isTalent ){
+            toast.info("Are you a talent or a recruiter?", toastOptions)
+        }else{
+            sendRegistrationData();
+        }
+            
     }
 
 
     return (
-        <form onClick={handleSubmit}>
-            <div>
-                <label>create your account</label>
-                <input type="text" name="firstName" placeholder="first name" onChange={handleInput}/>
-                <input type="text" name="lastName" placeholder="last name" onChange={handleInput}/>
-                <input type="email" name="email" placeholder="email" onChange={handleInput}/>
-            </div>
-            <div>
-                <input type="password" name="password" placeholder="password" onChange={handleInput}/>
-                <input type="password" placeholder="confirm password"/>
-            </div>
-            <form>
-                <input type="radio" name="whoAmI" value="isTalent" onChange={handleTalent}/>
-                <label>i am a talent</label>
-                <input type="radio" name="whoAmI" value="isRecruiter" onChange={handleRecruiter}/>
-                <label>i am a recruiter</label>
+        <>
+            <p>create your account</p>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <input type="text" name="firstName" placeholder="first name" required onChange={handleInput}/>
+                    <input type="text" name="lastName" placeholder="last name" required onChange={handleInput} />
+                    <input type="email" name="email" placeholder="email" required onChange={handleInput}/>
+                </div>
+                <div>
+                    <input type="password" name="password" placeholder="password" required onChange={handleInput}/>
+                    <input type="password" name="confirmPassword" placeholder="confirm password" required onChange={handleInput}/>
+                </div>
+                <div>
+                    <input type="radio" name="whoAmI" value="isTalent" onChange={handleTalent} />
+                    <label>i am a talent</label>
+                    <input type="radio" name="whoAmI" value="isRecruiter" onChange={handleRecruiter} />
+                    <label>i am a recruiter</label>
+                </div>
+                <button type="submit">register</button>
+                <div>
+                <p>already registered? 
+                <NavLink to="/login">click here</NavLink>
+                 </p>
+        </div>
             </form>
-            <button type="submit" >register</button>
             <ToastContainer />
-        </form>
+        </>
     )
 }
 
