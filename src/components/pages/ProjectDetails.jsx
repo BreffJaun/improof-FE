f1a3d8b476f35c08d423991515333ff022864aaf
Florian <f.mewes87@gmail.com>
2023-02-01 14:02:28 +0100

    ProjectDetails ausgebaut

--- src/components/pages/ProjectDetails.jsx
+++ src/components/pages/ProjectDetails.jsx
@@ -20,35 +20,60 @@ const ProjectDetails = () => {
         url: "https://www.downloadclipart.net/thumb/9394-stone-1-vector-thumb.png"
       }
     }
+  },
+  {
+    title: Date.now(),
+    cardTitle: "Stepstone",
+    cardSubtitle:"Timeline",
+    cardDetailedText: "today if build this Timeline with react Chrono",
+  }, 
+  {
+    title: Date.now(),
+    cardTitle: "Milestone",
+    cardSubtitle:"title",
+    cardDetailedText: "Description",
+    media: {
+      type: "IMAGE",
+      source: {
+        url: "https://www.downloadclipart.net/thumb/9394-stone-1-vector-thumb.png"
+      }
+    }
   }];
 
   return (
     <div className="componente">
+
       <div>
         <img src="https://www.downloadclipart.net/thumb/9394-stone-1-vector-thumb.png" alt="Thumbnail" />
         <h1>Project Name</h1>
         <p>description</p>
       </div>
       
+
      <Chrono items={items}/>
+
+
       <h1>Project Team</h1>
-     <div>
-      <div>
-        <img src="https://www.downloadclipart.net/thumb/9394-stone-1-vector-thumb.png" alt="" />
-        <h5>NAME</h5>
-        <h4>current Position</h4>
-      </div>
-      <div>
-        <img src="https://www.downloadclipart.net/thumb/9394-stone-1-vector-thumb.png" alt="" />
-        <h5>NAME</h5>
-        <h4>current Position</h4>
-      </div>
-      <div>
-        <img src="https://www.downloadclipart.net/thumb/9394-stone-1-vector-thumb.png" alt="" />
-        <h5>NAME</h5>
-        <h4>current Position</h4>
-      </div>
+     <div>      
+        <div>
+          <img src="https://www.downloadclipart.net/thumb/9394-stone-1-vector-thumb.png" alt="" />
+          <h5>NAME</h5>
+          <h4>current Position</h4>
+        </div>
+
+        <div>
+          <img src="https://www.downloadclipart.net/thumb/9394-stone-1-vector-thumb.png" alt="" />
+          <h5>NAME</h5>
+          <h4>current Position</h4>
+        </div>
+
+        <div>
+          <img src="https://www.downloadclipart.net/thumb/9394-stone-1-vector-thumb.png" alt="" />
+          <h5>NAME</h5>
+          <h4>current Position</h4>
+        </div>
      </div>
+
     </div>
   );
 };
