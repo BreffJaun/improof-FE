4bd66bdab1b819345482860ea84baa665efc74ee
magross <grossesbewirken@gmail.com>
2023-02-02 13:58:15 +0100

    sass export für florian

---
+++ src/components/elements/Avatar.jsx
@@ -0,0 +1,9 @@
+const AvatarL = () => {
+  return (
+    <div className="central">
+      <div className="circle70 bg-FAV"></div>
+    </div>
+  );
+};
+
+export default AvatarL;
--- src/components/pages/MyProfil.jsx
+++ src/components/pages/MyProfil.jsx
@@ -2,6 +2,7 @@ import Footer from "../elements/Footer.jsx";
 import { ColorCardRecruiter, ColorCardTalent } from "../elements/ColorCard.jsx";
 import { GiThreeFriends } from "react-icons/gi";
 import { TalentCardFollow } from "../elements/TalentCard.jsx";
+import AvatarL from "../elements/Avatar.jsx";
 
 const user = {friends:[
   {name:"UWE", currentPosition:"WebDev", initials:"UK" , img:"https://streunerglueck.de/wp-content/uploads/2020/04/Katzen1.jpg"},
@@ -12,9 +13,32 @@ const user = {friends:[
 const MyProfil = () => {
   return (
     <div className="mb2 mt2">
-      <h1 className="central c-FAV mb2">my profil</h1>
-      <ColorCardTalent/>
-      <ColorCardRecruiter/>
+      <h1 className="central c-FAV mb2">username</h1>
+
+      <>
+        {/* talentprofil */}
+        <AvatarL/>
+        <ColorCardTalent/>
+        <div>
+          <p className="c-FAV">that's me:</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, aliquam.</p>
+          <button>read more</button>
+        </div>
+        <div className="bo-DARK"></div>
+
+      </>
+
+
+      <>
+        {/* talentprofil */}
+        <AvatarL/>
+        <ColorCardRecruiter/>
+      </>
+      
+
+
+      
+      
 
       {user.friends.map(friend => <TalentCardFollow name={friend.name} position={friend.currentPosition} img={friend.img} initials={friend.initials} />)}
       
