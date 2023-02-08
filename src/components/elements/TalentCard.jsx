3ebf79055c9ed733eb96d30905c8a98c622d12d4
grossesbewirken <grossesbewirken@gmail.com>
2023-02-08 16:25:20 +0100

    talent card message field in progress

--- src/components/elements/TalentCard.jsx
+++ src/components/elements/TalentCard.jsx
@@ -10,28 +10,38 @@ import { ToastContainer } from "react-toastify"
 import { FollowBtn } from "../buttons/FollowBtn.jsx"
 import { SendMessageBtn } from "../buttons/MessageBtn.jsx"
 
+import { FiSend } from "react-icons/fi"
+
+
+
+
 const TalentCard = ({talent, user}) => {
   
   const navigate = useNavigate()
   return (
-    <div className="card talent flex" >
-      <div className="circle50 bg-FAV central" onClick={()=> navigate(`/userDetails/${talent._id}`)}>
-        { talent?.profile?.avatar ? 
-          <img src={talent.profile.avatar} width="50" /> : 
-          <p className="initials">{talent.profile?.initials}</p> 
-}
-      </div>
-      <div>
-        <p onClick={()=> navigate(`/userDetails/${talent._id}`)}>{talent.profile.firstName} {talent.profile.lastName}</p>
-        <p className="c-A80">{talent.profile.position}</p>
-        <p className="c-A80">{talent.profile.toolsAndSkills}</p>
-      </div>
-      <div className="flex">
-        <SendMessageBtn talent={talent} user={user} />
-        {user.follows.find(follow => follow._id === talent._id) ? <FollowBtn talent={talent} user={user}/> : <FollowBtn talent={talent} user={user}/>}
+    <>
+      <div className="card talent" >
+        <div className="flex central">
+          <div className="circle50 bg-FAV central" onClick={()=> navigate(`/userDetails/${talent._id}`)}>
+            { talent?.profile?.avatar ? 
+              <img src={talent.profile.avatar} width="50" /> : 
+              <p className="initials">{talent.profile?.initials}</p> }
+          </div>
+          <div className="ml1 col">
+            <p onClick={()=> navigate(`/userDetails/${talent._id}`)}>{talent.profile.firstName} {talent.profile.lastName}</p>
+            <p className="c-A80">{talent.profile.position}</p>
+            <p className="c-A80">{talent.profile.toolsAndSkills}</p>
+          </div>
+        </div>
+
+        <div className="flex">
+          <button className="circle30"><FiSend /></button>
+          <SendMessageBtn talent={talent} user={user} />
+          {user.follows.find(follow => follow._id === talent._id) ? <FollowBtn talent={talent} user={user}/> : <FollowBtn talent={talent} user={user}/>}
+        </div>
       </div>
       <ToastContainer/>
-    </div>
+    </>
   );
 };
 
