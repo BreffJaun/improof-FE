b7e2d044a618e22feacdb689a35dac4b709501e0
Florian <f.mewes87@gmail.com>
2023-02-08 15:36:37 +0100

    notification date angepasst

--- src/components/pages/Notifications.jsx
+++ src/components/pages/Notifications.jsx
@@ -14,11 +14,14 @@ const Notifications = () => {
       <div className="central col">
         <h1 className="c-FAV mt2 mb2">notifications</h1>
         <div className="col">
-          {user.notifications.map(notification => 
-            <div className="mb1" key ={notification._id}>
+          {user.notifications.map(notification => {     
+              const date1 = notification.createdAt.toString().split("T")
+              const date = date1[0].split("-").reverse().join(".")
+              const time = date1[1].slice(0,5)            
+              return <div className="mb1" key ={notification._id}>
                 <p className="c-FAV">{notification.notText}</p>
-                <p>{notification.createdAt}</p>
-            </div>).reverse()}
+                <p>{date + " " +  time}</p>
+            </div>}).reverse()}          
         </div>
       </div>
       <Footer/>
