b00517d24de07e26aa612b420517c44a14deeb93
BreffJaun <braun_jeff@web.de>
2023-02-09 10:26:07 +0100

    New Search gebastelt

--- src/components/pages/NewSearch.jsx
+++ src/components/pages/NewSearch.jsx
@@ -3,13 +3,14 @@
 import { NavLink, useNavigate } from "react-router-dom";
 import { useEffect, useState, useContext } from "react";
 import { toast, ToastContainer } from "react-toastify";
-import { Map, Marker, ZoomControl, Overlay } from "pigeon-maps";
+import { Map, Marker, ZoomControl, Overlay, GeoJson, GeoJsonFeature} from "pigeon-maps";
 import Geocode from "react-geocode";
 
 // I M P O R T:  F I L E S   &   F U N C T I O N S
 import Footer from "../elements/Footer.jsx";
 import { host } from "../../api/host.jsx";
 import UserContext from "../../context/userContext.jsx";
+import { TalentCard } from "../elements/TalentCard.jsx";
 
 // I M P O R T:  K E Y S
 import {MAP_BOX_URL, MAP_BOX_ENDPOINT, MAP_BOX_KEY} from "../../api/mapBoxApiKeys.jsx";
@@ -19,24 +20,26 @@ import {MAP_BOX_URL, MAP_BOX_ENDPOINT, MAP_BOX_KEY} from "../../api/mapBoxApiKey
 
 const NewSearch = () => {
   const navigate = useNavigate();
-  // const search = {
-  //   position: "",
-  //   tas: "",
-  //   zip: "",
-  //   radius: ""
-  // }
+  const search = {
+    position: "",
+    tas: "",
+    zip: "",
+    radius: ""
+  }
   const [user, setUser] = useContext(UserContext);
   const [isLoading, setLoading] = useState(true);
   const [talents, setTalents] = useState(undefined);
   const [updatedTalents, setUpdatedTalents] = useState(undefined);
   const [currTalent, setCurrTalent] = useState(undefined);
-  const [searchData, setSearchData] = useState(undefined);
+  const [searchData, setSearchData] = useState(search);
   const [searchTrigger, setSearchTrigger] = useState(false);
+  const [redMarker, setRedMarker] = useState(false);
   const toastOptions = {
       position: "bottom-right",
       autoClose: 8000,
       theme: "dark",
   };
+  let talentsToMap;
 
   useEffect(() => {
     // FETCH ALL TALENTS FOR THE MAP & FETCH LAT. & LON. FROM MAP BOX & SET IN USERS
@@ -78,8 +81,19 @@ const NewSearch = () => {
   const handleInput = (event) => {
     setSearchData({...searchData, [event.target.name]: event.target.value});
   };
+
+  const searchTriggerHandler = () => {
+    setSearchTrigger(!searchTrigger);
+  }
+
+  const redMarkerHandler = () => {
+    setRedMarker(!redMarker);
+  }
+  
   const handleSubmit = (event) => {
     event.preventDefault();
+    const position = searchData.position;
+    const tas = searchData.tas;
     const zip = searchData.zip;
     const radius = searchData.radius;
     const SEARCH_TEXT = `${zip}`;
@@ -91,6 +105,8 @@ const NewSearch = () => {
         const lon = json.features[0].center[0];
         const newSearchData = {...searchData, latitude: lat, longitude: lon};
         setSearchData(newSearchData); 
+        setSearchTrigger(true);
+        setRedMarker(true);
         // SET SEARCH DATA & ADD LONG.- & LATITUDE END //
         // CALCULATE & FILTER DISTANCE WITH GEOCODES START //
         const talentsWithDistance = talents.map((talent, i) => { 
@@ -102,19 +118,58 @@ const NewSearch = () => {
           );
           return {...talent, location: {...talent.location, distance: dist}};
         });
+        setUpdatedTalents(talentsWithDistance); 
         // console.log('talentsWithDistance: ', talentsWithDistance)
+
+        // FILTER TALENTS ON DISTANCE
         const talentsSortedByDistance = talentsWithDistance.filter((talent) => !Object.is(talent.location.distance, NaN)).filter((talent) => talent.location.distance < radius).sort(
           (a, b) => a.location.distance - b.location.distance
         );
         setUpdatedTalents(talentsSortedByDistance);
         // console.log('talentsSortedByDistance: ', talentsSortedByDistance)
-      })
+      })     
       .catch(error => console.log(searchData, error))
     }
     zip && radius && getSearchData();
     // CALCULATE & FILTER DISTANCE WITH GEOCODES END //
+    
+    // POSITION FILTER
+    if(updatedTalents && position) {
+      setSearchTrigger(true)
+      console.log('POSITION IF')
+      setUpdatedTalents(updatedTalents.filter((talent) => talent.profile.position.includes(position)))
+      // talentsToMap = updatedTalents.filter((talent) => talent.profile.position.includes(position));
+      console.log(talentsToMap)
+    } else if (position) {
+      setSearchTrigger(true)
+      console.log('POSITION ELSE')
+      setUpdatedTalents(talents.filter((talent) => talent.profile.position.includes(position)))
+      // console.log(talentsToMap)
+    }
+
+    // TOOLS & SKILLS FILTER
+    if(updatedTalents && tas) {
+      setSearchTrigger(true)
+      console.log('TAS IF')
+      setUpdatedTalents(updatedTalents.filter((talent) => talent.profile.toolsAndSkills.includes(tas)));
+      // console.log(talentsToMap)
+    } else if (tas) {
+      setSearchTrigger(true)
+      console.log('TAS ELSE')
+      setUpdatedTalents(talents.filter((talent) => talent.profile.toolsAndSkills.includes(tas)));
+      // console.log(talentsToMap)
+    }
+    
+
   };
-  updatedTalents && console.log('updatedTalents: ', updatedTalents)
+  // updatedTalents && console.log('updatedTalents: ', updatedTalents)
+
+  // SET THE RIGHT TALENT SOURCE TO map OVER FOR THE "MAP"
+  if(updatedTalents) {
+    talentsToMap = updatedTalents
+  } else {
+    talentsToMap = talents
+  }
 
   // FUNCTIONS TO CALCULATE DISTANCE START //
   function deg2rad(deg) {
@@ -154,12 +209,30 @@ const NewSearch = () => {
 //     });
   
 
-  // SHOW PIN OVERLAYS
+// SHOW PIN OVERLAYS
+const overlayHandler = (e, talent) => {
+  setCurrTalent(talent);
+};
   
-  const overlayHandler = (e, talent) => {
-    setCurrTalent(talent);
-  };
-  
+// RESET SEARCH START //
+const resetHandler = () => {
+  setUpdatedTalents(undefined);
+  setSearchData(search);
+  setCurrTalent(undefined);
+  setSearchTrigger(false);
+  setRedMarker(false)
+};
+console.log('searchTrigger: ', searchTrigger)
+console.log('redMarker: ', redMarker)
+
+// RESET SEARCH END //
+
+// const geoJsonFeatureSample = {
+//   type: "Feature",
+//   geometry: { type: "Point", coordinates: [2.0, 48.5] },
+//   properties: { prop0: "value0" },
+// };
+
   return !isLoading && (
     < div className="componente">
         <div className="mb2 mt2">
@@ -173,13 +246,18 @@ const NewSearch = () => {
                         name="position"
                         placeholder="what do you want to achieve"
                         onChange={handleInput}
+                        disabled={searchTrigger}
+                        value={searchData.position}
+                        
                     />
                     <p>tools & skills</p>
                     <input 
                         type="text" 
                         name="toolsAndSkills" 
                         placeholder="what is the talents set" 
-                        onChange={handleInput} 
+                        onChange={handleInput}
+                        disabled={searchTrigger} 
+                        value={searchData.tas}
                     />
                     <p>ZIP-Code</p>
                     <input 
@@ -187,19 +265,38 @@ const NewSearch = () => {
                         name="zip" 
                         placeholder="type in a number" 
                         onChange={handleInput}
+                        disabled={searchTrigger}
+                        value={searchData.zip}
                     />
                     <p>search radius (km)</p>
                     <input 
-                        type="number" 
+                        style={{cursor:'pointer'}}
+                        type="text" 
                         name="radius" 
                         placeholder="type in a number" 
                         onChange={handleInput}
+                        disabled={searchTrigger}
+                        value={searchData.radius}
                     />
                 </div>
                 <div className="central mb2 mt2">
-                    <button type="submit" className="bg-FAV">get result</button>
-                </div>
-            </form>
+                    <button 
+                      style={{cursor:'pointer'}}
+                      type="submit" 
+                      className="bg-FAV"
+                      disabled={searchTrigger}
+                    >get result</button>
+                </div> 
+                <div className="central mb2 mt2">
+                    <button 
+                      style={{cursor:'pointer'}}
+                      type="reset" 
+                      className="bg-FAV"
+                      disabled={!searchTrigger}
+                      onClick={resetHandler}
+                    >reset search</button>
+                </div>        
+              </form>
             <div>
               
               {(talents) && (
@@ -213,8 +310,8 @@ const NewSearch = () => {
                 ]}
                 defaultZoom={5} // je größer die Zahl, desto weiter rein gezoomt
               >
-            
-              {searchData ?
+              {/* RED MARKER FOR ZIP */}
+              {redMarker ?
                 <Marker
                   width={30}
                   color={"red"}
@@ -222,19 +319,22 @@ const NewSearch = () => {
                 /> :
                 <></>             
               }
-              
-              {talents?.map((talent) => {
-                return (
-                  <Marker
-                    width={30}
-                    anchor={[
-                      +talent.location.latitude,
-                      +talent.location.longitude,
-                    ]}
-                    key={talent._id}
-                    onClick={(e) => overlayHandler(e, talent)}
-                  />
-                );
+
+              {/* MAP ABOUT UPDATED TALENTS => IF THEY AREN´T SET, MAP ABOUT TALENTS */}
+              {talentsToMap.map((talent) => {
+                if(talent.location.latitude) {
+                  return (
+                    <Marker
+                      width={30}
+                      anchor={[
+                        +talent.location.latitude,
+                        +talent.location.longitude,
+                      ]}
+                      key={talent._id}
+                      onClick={(e) => overlayHandler(e, talent)}
+                    />
+                  );
+                } 
               })}
               {currTalent && (
                 <Overlay
@@ -250,8 +350,34 @@ const NewSearch = () => {
                 </Overlay>
               )}
                 <ZoomControl />
+                {/* <GeoJson
+                  svgAttributes={{
+                    fill: "#d4e6ec99",
+                    strokeWidth: "1",
+                    stroke: "white",
+                    r: "20",
+                  }}
+                >
+                  <GeoJsonFeature feature={geoJsonFeatureSample} />
+                </GeoJson> */}
               </Map>
               )}
+            </div>
+            {/* CURR TALENT */}
+            {currTalent && (
+              <div>
+                <h1>SELECTED TALENT</h1>
+                <TalentCard talent={currTalent} user={user} key={currTalent._id}/>
+                <div className="bo-DARK"></div>
+              </div>
+            )}
+            {/* SEARCHED TALENTS (OR ALL TALENTS) */}
+            <div>
+              {updatedTalents ? (<h1>FILTERED TALENTS</h1>) : (<h1>TALENTS</h1>)}
+              {talentsToMap.map((talent) => {
+                return (
+                <TalentCard talent={talent} user={user} key={talent._id}/>
+              )})}
             </div>
             <Footer/>
             <ToastContainer />
