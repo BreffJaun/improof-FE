9c50e15bee8732cff9a8ee69f960f603f6e00001
elenosis <eleni.orfanou.88@gmail.com>
2023-02-17 13:53:46 +0100

    editStone pull

--- src/components/elements/StoneCard.jsx
+++ src/components/elements/StoneCard.jsx
@@ -2,7 +2,29 @@ import { useState, useEffect, useContext } from "react";
 import { host } from "../../api/host.jsx";
 
 const StoneCard = () => {
-  return <div className="news-container">stoneCard</div>;
+  const { stoneId } = useParams("stoneId");
+  const [stone, setStone] = useState({});
+
+  useEffect(() => {
+    setPending(true);
+    const fetchStone = async () => {
+      fetch(`${host}/stones/${stoneId}`, {
+        credentials: "include",
+      })
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json);
+          if (json.status) {
+            setStone(json.data);
+            const newCon = json.data.team.map((con) => con._id);
+            setContributors(newCon);
+            setPending(false);
+          }
+        });
+    };
+    fetchStone();
+  });
+  return <div className="news-container">${stone}</div>;
 };
 
 export default StoneCard;
