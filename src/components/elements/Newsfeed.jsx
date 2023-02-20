8795d7a920a2e292a3a90a2ee80baa8e2ef30b23
elenosis <eleni.orfanou.88@gmail.com>
2023-02-20 09:41:27 +0100

    auto slides

--- src/components/elements/Newsfeed.jsx
+++ src/components/elements/Newsfeed.jsx
@@ -9,45 +9,45 @@ import {
 } from "pure-react-carousel";
 import "pure-react-carousel/dist/react-carousel.es.css";
 
-
 // ELEMENTE
-import NewsCard from "./NewsCard"
+import NewsCard from "./NewsCard";
 import CategoriesFilter from "./CategoriesFilter.jsx";
-
 
 // CONTEXT
 import TriggerContext from "../../context/triggerContext.jsx";
 import UserContext from "../../context/userContext.jsx";
 
-
-import { IoIosArrowBack as Back } from "react-icons/io"
-import {IoIosArrowForward as Forward} from "react-icons/io"
-
+import { IoIosArrowBack as Back } from "react-icons/io";
+import { IoIosArrowForward as Forward } from "react-icons/io";
 
 const Newsfeed = () => {
   const [projects, setProjects] = useState([]);
   const [starProjects, setStarProjects] = useState([]);
   const [user, setUser] = useContext(UserContext);
   const [category, setCategory] = useState("");
-  const [sortedList, setSortedList] = useState(projects)
+  const [sortedList, setSortedList] = useState(projects);
 
-  const color = user.meta.colorTheme[0]
-  const bg = user.meta.colorTheme[1]
+  const color = user.meta.colorTheme[0];
+  const bg = user.meta.colorTheme[1];
 
-  useEffect(()=>{
-    const sorted = projects.sort((a,b)=> {
-      if(a.updatedAt && b.updatedAt){
-        let x = a.updatedAt
-        let y = b.updatedAt
-        if (x < y) {return -1;}
-        if (x > y) {return 1;}
+  useEffect(() => {
+    const sorted = projects.sort((a, b) => {
+      if (a.updatedAt && b.updatedAt) {
+        let x = a.updatedAt;
+        let y = b.updatedAt;
+        if (x < y) {
+          return -1;
+        }
+        if (x > y) {
+          return 1;
+        }
         return 0;
       }
-      return 0
-    })
-    const short = sorted.reverse().splice(0,10)
-    setSortedList(short)
-  },[projects])
+      return 0;
+    });
+    const short = sorted.reverse().splice(0, 10);
+    setSortedList(short);
+  }, [projects]);
 
   useEffect(() => {
     const getProjects = async () => {
@@ -61,16 +61,16 @@ const Newsfeed = () => {
     };
     getProjects();
 
-    const starProjectIds = user.starProjects.map(pro => pro._id)
+    const starProjectIds = user.starProjects.map((pro) => pro._id);
     const getStarProjects = async () => {
       fetch(`${host}/projects/news`, {
         credentials: "include",
-        method:"POST",
+        method: "POST",
         body: JSON.stringify(starProjectIds),
         headers: {
-          'Content-type': 'application/json; charset=UTF-8',
+          "Content-type": "application/json; charset=UTF-8",
         },
-      })      
+      })
         .then((response) => response.json())
         .then((json) => {
           setStarProjects(json.data);
@@ -91,53 +91,61 @@ const Newsfeed = () => {
           totalSlides={sortedList.length}
           infinite={true}
           lockOnWindowScroll={true}
+          isPlaying={true}
         >
           <Slider>
             {category === undefined ||
             category === "all categories" ||
-            category === "" || !category
-              ? sortedList?.map((project,i) => {
-                  return(
-                  <Slide index={i} key={project._id}>
-                    <NewsCard
-                      key={project._id}
-                      user={user}
-                      project={project}
-                    />
-                  </Slide>
-                  )
-                })
-              : sortedList?.map((project,i) =>{            
-                return category === project.category &&            
-                  <Slide index={i} key={project._id}>
-                    <NewsCard
-                      key={project._id}
-                      user={user}
-                      project={project}
+            category === "" ||
+            !category
+              ? sortedList?.map((project, i) => {
+                  return (
+                    <Slide index={i} key={project._id}>
+                      <NewsCard
+                        key={project._id}
+                        user={user}
+                        project={project}
                       />
-                  </Slide>
-              
-                
-
-              }                
-                )}
+                    </Slide>
+                  );
+                })
+              : sortedList?.map((project, i) => {
+                  return (
+                    category === project.category && (
+                      <Slide index={i} key={project._id}>
+                        <NewsCard
+                          key={project._id}
+                          user={user}
+                          project={project}
+                        />
+                      </Slide>
+                    )
+                  );
+                })}
           </Slider>
           <div className="central">
-            <ButtonBack className={bg}><h3><Back /></h3></ButtonBack>
+            <ButtonBack className={bg}>
+              <h3>
+                <Back />
+              </h3>
+            </ButtonBack>
             <div className="central col">
               <p className="mb1">choose your topic</p>
               <CategoriesFilter category={category} setCategory={setCategory} />
             </div>
-            <ButtonNext className={bg}><h3><Forward/></h3></ButtonNext>
+            <ButtonNext className={bg}>
+              <h3>
+                <Forward />
+              </h3>
+            </ButtonNext>
           </div>
-        </CarouselProvider>        
+        </CarouselProvider>
       </div>
 
       <div className="bo-DARK"></div>
       <div>
         <h1 className={`${color} center mt1`}>newsfeed</h1>
         <div className="">
-
           <div>
             {/* {starProjects.map(project => 
             <div key={project._id}>
@@ -146,37 +154,49 @@ const Newsfeed = () => {
               <NewsCard project={project._id} user={user}/>
             </div>
             )} */}
-            {starProjects.map(project => {
-              return project.stones.sort((a,b)=> {
-                if(a.createdAt && b.createdAt){
-                  let x = a.createdAt
-                  let y = b.createdAt
-                  if (x < y) {return 1;}
-                  if (x > y) {return -1;}
+            {starProjects.map((project) => {
+              return project.stones
+                .sort((a, b) => {
+                  if (a.createdAt && b.createdAt) {
+                    let x = a.createdAt;
+                    let y = b.createdAt;
+                    if (x < y) {
+                      return 1;
+                    }
+                    if (x > y) {
+                      return -1;
+                    }
+                    return 0;
+                  }
                   return 0;
-                }
-                return 0
-              }).map(stone => {
-                return (
-                  <div>
+                })
+                .map((stone) => {
+                  return (
                     <div>
-                      {stone.team.map(member =>
                       <div>
-                        {/* <img className="circle50" src={member.profile.avatar}></img> */}
-                        <p><img className="circle50" src={member.profile.avatar}></img>{member.profile.firstName} has created a new {stone.kind} in {project.name} at {stone.createdAt}</p>
-                        <NewsCard project={project} user={user}/>
-                      </div>            
-                      )}
+                        {stone.team.map((member) => (
+                          <div>
+                            {/* <img className="circle50" src={member.profile.avatar}></img> */}
+                            <p>
+                              <img
+                                className="circle50"
+                                src={member.profile.avatar}
+                              ></img>
+                              {member.profile.firstName} has created a new{" "}
+                              {stone.kind} in {project.name} at{" "}
+                              {stone.createdAt}
+                            </p>
+                            <NewsCard project={project} user={user} />
+                          </div>
+                        ))}
+                      </div>
                     </div>
-                  </div>
-                )               
-              })
+                  );
+                });
             })}
-         
           </div>
         </div>
       </div>
-
     </div>
   );
 };
