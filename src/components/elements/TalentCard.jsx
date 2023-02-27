86367fb1692f6b594da406708871c694bc30256d
grossesbewirken <grossesbewirken@gmail.com>
2023-02-27 23:31:45 +0100

    userdetails und talentcard prettier

--- src/components/elements/TalentCard.jsx
+++ src/components/elements/TalentCard.jsx
@@ -39,13 +39,14 @@ const TalentCard = ({ talent, user }) => {
           </div>
         </div>
         <div className="t-info">
-          <p
-            className={`fw500 ${color}`}
+          <h4
+            className={`fw900 ${color}`}
             onClick={() => navigate(`/userDetails/${talent._id}`)}
           >
             {talent.profile?.firstName} {talent.profile?.lastName}
-          </p>
+          </h4>
           <p>{talent.profile?.category}</p>
+          <p className={`mt1 info c-A50`}>tools and skills </p>
           <p>{talent.profile?.toolsAndSkills}</p>
         </div>
 
