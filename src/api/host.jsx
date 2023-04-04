f9f875b7c091525a2ea6541e7d1638bfd2cdc6cc
BreffJaun <braun_jeff@web.de>
2023-04-04 10:14:09 +0200

    Änderung host zuürck auf BE Lochalost 2404

--- src/api/host.jsx
+++ src/api/host.jsx
@@ -1,4 +1,4 @@
 // const host = "https://improof-be.onrender.com";
 // const host = "https://improof-be-atur.onrender.com";
-const host = "http://localhost:5173";
+const host = "http://localhost:2404";
 export { host };
