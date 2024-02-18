12584b20fd2a436d29e925e75b9e2a56d71b723a
BreffJaun <braun_jeff@web.de>
2024-02-18 14:44:18 +0100

    Changed host.jsx for slightly change from local work to push new updates on IONOS

--- src/api/host.jsx
+++ src/api/host.jsx
@@ -1,4 +1,13 @@
 // const host = "https://improof-be.onrender.com";
 // const host = "https://improof-be-atur.onrender.com";
-const host = "/api";
+
+// Vor dem Push & Pull auf IONOS nginx server diese Zeile wieder auskommentieren.
+// Für die lokale Arbeit einkommentieren
+const host = "https://improof-be-atur.onrender.com/api";
+
+// Vor dem Push & Pull auf IONOS nginx server diese Zeile wieder einkommentieren.
+// Für die lokale Arbeit auskommentieren
+// const host = "/api";
+
+// Diese Zeile bitte in Ruhe lassen 🙂
 export { host };
